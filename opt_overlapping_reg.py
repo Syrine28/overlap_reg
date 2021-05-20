5015dc92799d926b7d85edadcf3e6218f19ec522
Syrine28 <84201132+Syrine28@users.noreply.github.com>
2021-05-20 12:13:34 -0400

    Update opt_overlapping_reg.py

--- opt_overlapping_reg.py
+++ opt_overlapping_reg.py
@@ -48,6 +48,8 @@ def sweep_and_compare(tex1, tex2, crop1, crop2, threshold) :
     
     min_loss_fct = loss_fct(overlap1, overlap2)
     best_overlap1 = overlap1
+    best_overlap2 = overlap2
+
 
     p = 0
     shift_array = []
@@ -57,7 +59,7 @@ def sweep_and_compare(tex1, tex2, crop1, crop2, threshold) :
     while (crop2 - p > threshold) :
         overlap1_sw = tex1[0:height1, crop1 + p:width1].copy()
         overlap2_sw = tex2[0:height2, 0:crop2 - p].copy()
-
+                    
         curr_loss_fct = loss_fct(overlap1_sw, overlap2_sw)
         loss_fct_array.append(curr_loss_fct)
         
@@ -73,15 +75,48 @@ def sweep_and_compare(tex1, tex2, crop1, crop2, threshold) :
         if (curr_loss_fct < min_loss_fct) :
             min_loss_fct = curr_loss_fct
             best_overlap1 = overlap1_sw
-        
-        p = p + 25
+            best_overlap2 = overlap2_sw
+                    
+        # This is just to get pics of the sweeped areas
+        #wd = os.getcwd()
+        #cv2.imshow(wd + "\sweeped_reg", overlap1_sw)
+        #cv2.waitKey(0)
+
+        p = p + 100
     
     wd = os.getcwd()
-
     cv2.imshow(wd + "\best_overlap00", best_overlap1)
-    #or just min_overlap1
     cv2.waitKey(0) 
     cv2.destroyAllWindows() 
+    
+    return best_overlap1, best_overlap2
+
+def align_images(tex1, tex2, overlap1, overlap2) :
+    #We'll blur out the overlapping region in img2 then superimpose tex1 on top of tex2
+    #blurred_overlap2 = cv2.blur(overlap2, (r,r))
+    
+    #We'll blend both overlaps together
+    blended = cv2.addWeighted(overlap1, 0.3, overlap2, 0.7, 0.0)
+    
+    #Now we paste tex1 without the overlap to blended
+    width_over = overlap1.shape[1]  # = overlap1.shape[1]
+    height1 = tex1.shape[0]
+    width1 = tex1.shape[1]
+    tex1_wo_overlap = tex1[0:height1, 0:width1 - width_over].copy()
+    partial_img = np.concatenate((tex1_wo_overlap, blended), axis=1)
+
+    #Now paste it to tex2 without the overlap to get the final image
+    height2 = tex2.shape[0]
+    width2 = tex2.shape[1]
+    tex2_wo_overlap = tex2[0:height2, width_over:width2].copy()
+    
+    final_img = np.concatenate((partial_img, tex2_wo_overlap), axis=1)
+    wd = os.getcwd()
+    cv2.imshow(wd + "\aligned_textures", final_img)
+    cv2.waitKey(0) 
+    cv2.destroyAllWindows()     
+    
+    return final_img
 
 #img1 = Image.open("/Users/syrineenneifer/Desktop/oak_back_00_heightmap_nrm.png")
 #overlap_reg1 = img1.crop((4150, 0, 6024, 4022))
@@ -102,4 +137,6 @@ img1 = cv2.imread(wd + "\oak_back_00_heightmap_nrm.png")
 
 img2 = cv2.imread(wd + "\oak_back_01_heightmap_nrm.png")
 
-sweep_and_compare(img1, img2, 4150, 1874, 10)
+opt_overlap1, opt_overlap2 = sweep_and_compare(img1, img2, 4150, 1874, 10)
+
+align_images(img1, img2, opt_overlap1, opt_overlap2)
