50b7c0652b8eb098a76092652a60978e57cfdf69
Syrine28 <84201132+Syrine28@users.noreply.github.com>
2021-06-03 14:25:04 -0400

    Update opt_overlapping_reg.py

--- opt_overlapping_reg.py
+++ opt_overlapping_reg.py
@@ -21,10 +21,10 @@ def loss_fct(matrix_ov_reg1, matrix_ov_reg2) :
         n2 = normal_vector(convert_to_array(matrix_ov_reg2[i]))
         
         # FIRST METHOD : 
-        #error = np.linalg.norm(n1 - n2) 
+        error = np.linalg.norm(n1 - n2) 
         
         # SECOND METHOD :
-        error = 1 - np.dot(n1, n2)
+        #error = 1 - np.dot(n1, n2)
         
         # THIRD METHOD :
         #error = mae(n1, n2)
@@ -32,33 +32,32 @@ def loss_fct(matrix_ov_reg1, matrix_ov_reg2) :
         sum += error
     
     # FOR FIRST AND THIRD METHOD
-    #return sum/(len(matrix_ov_reg1)) # len(matrix_ov_reg1) = # of pixels (same as reg2)
+    return sum/(len(matrix_ov_reg1)) # len(matrix_ov_reg1) = # of pixels (same as reg2)
    
     # FOR SECOND METHOD    
-    return sum
+    #return sum
 
-def sweep_and_compare(tex1, tex2, crop1, crop2, threshold) :
+#ONLY FOR HORIZONTAL OVERLAPPING
+def sweep_and_compare(tex1, tex2, crop1, crop2) :
     height1 = tex1.shape[0]
     width1 = tex1.shape[1]
     overlap1 = tex1[0:height1, crop1:width1].copy()  #here, crop1 = 4150
     
     height2 = tex2.shape[0]
-    width2 = tex2.shape[1]
     overlap2 = tex2[0:height2, 0:crop2].copy() #here, crop2 = 1874
     
     min_loss_fct = loss_fct(overlap1, overlap2)
     best_overlap1 = overlap1
     best_overlap2 = overlap2
 
-
     p = 0
     shift_array = []
     shift = -1
     loss_fct_array = []
     
-    while (crop2 - p > threshold) :
+    while (crop1 + p < width1) :
         overlap1_sw = tex1[0:height1, crop1 + p:width1].copy()
-        overlap2_sw = tex2[0:height2, 0:crop2 - p].copy()
+        overlap2_sw = tex2[0:height2, p:crop2].copy()
                     
         curr_loss_fct = loss_fct(overlap1_sw, overlap2_sw)
         loss_fct_array.append(curr_loss_fct)
@@ -82,7 +81,7 @@ def sweep_and_compare(tex1, tex2, crop1, crop2, threshold) :
         #cv2.imshow(wd + "\sweeped_reg", overlap1_sw)
         #cv2.waitKey(0)
 
-        p = p + 1
+        p = p + 50
    
     return best_overlap1, best_overlap2
 
@@ -95,6 +94,7 @@ def align_images_hor(tex1, tex2, overlap1, overlap2) :
     height1 = tex1.shape[0]
     width1 = tex1.shape[1]
     tex1_wo_overlap = tex1[0:height1, 0:width1 - width_over].copy()
+    
     #axis=1 is to concatenate them horizontally
     partial_img = np.concatenate((tex1_wo_overlap, blended), axis=1)
 
@@ -116,6 +116,7 @@ def align_images_ver(tex1, tex2, overlap1, overlap2) :
     height1 = tex1.shape[0]
     width1 = tex1.shape[1]
     tex1_wo_overlap = tex1[height_over:height1, 0:width1].copy()
+
     #axis=0 is to concatenate them vertically
     partial_img = np.concatenate((tex1_wo_overlap, blended), axis=0)
 
@@ -138,16 +139,21 @@ def align_right_and_down(img, img_right, img_down, overlap_right1, overlap_right
     return final_right, final_down   
     
 #This function will take as input all the scans that are on the same row (IDK how many there are =: x)
-def align_row(img1, img2, img3, overlap1, overlap2_1, overlap2_3, overlap3) :
+def align_row(img1, img2, img3, img4, img5, img6, img7,
+              overlap1, overlap2_1, overlap2_3, overlap3_2,
+              overlap3_4, overlap4_3, overlap4_5, overlap5_4,
+              overlap5_6, overlap6_5, overlap6_7, overlap7) :
     #For a whole row 
      
     #Find overlap between the 2 pics, blend the two same overlaps
     #Remove it from the original scans then concatenate them to the overlap
     partial1 = align_images_hor(img1, img2, overlap1, overlap2_1)
-    partial2 = align_images_hor(partial1, img3, overlap2_3, overlap3)
-    
-    final = partial2
-    #Do that for all pics and overlaps
+    partial2 = align_images_hor(partial1, img3, overlap2_3, overlap3_2)
+    partial3 = align_images_hor(partial2, img4, overlap3_4, overlap4_3)
+    partial4 = align_images_hor(partial3, img5, overlap4_5, overlap5_4)
+    partial5 = align_images_hor(partial4, img6, overlap5_6, overlap6_5)
+    final = align_images_hor(partial5, img7, overlap6_7, overlap7)
+
     #Too long???? 
         
     #Then return the last partial = final
@@ -155,26 +161,66 @@ def align_row(img1, img2, img3, overlap1, overlap2_1, overlap2_3, overlap3) :
     return final
 
 wd = os.getcwd()
+
+# oak 
+
 img1 = cv2.imread(wd + "\oak_back_00_heightmap_nrm.png")
  
 img2 = cv2.imread(wd + "\oak_back_01_heightmap_nrm.png")
 
 img3 = cv2.imread(wd + "\oak_back_02_heightmap_nrm.png")
 
-opt_overlap1, opt_overlap2_1 = sweep_and_compare(img1, img2, 4150, 1874, 10)
-opt_overlap2_3, opt_overlap3 = sweep_and_compare(img2, img3, 4150, 1874, 10)
+img4 = cv2.imread(wd + "\oak_back_03_heightmap_nrm.png")
 
-cv2.imshow("opt_overlap1", opt_overlap1)
-cv2.imwrite("opt_overlap1.png", opt_overlap1)
+img5 = cv2.imread(wd + "\oak_back_04_heightmap_nrm.png")
+
+img6 = cv2.imread(wd + "\oak_back_05_heightmap_nrm.png")
+
+img7 = cv2.imread(wd + "\oak_back_06_heightmap_nrm.png")
+
+
+# acrylic 
+"""
+img1 = cv2.imread("acrylic00.png")
+
+img2 = cv2.imread("acrylic01.png")
+
+img3 = cv2.imread("acrylic02.png")
+
+img4 = cv2.imread("acrylic03.png")
+
+img5 = cv2.imread("acrylic04.png")
+
+img6 = cv2.imread("acrylic05.png")
+
+img7 = cv2.imread("acrylic06.png")
+"""
+
+# FOR OAK : crop1 = 4150, crop2 = 1874
+# FOR ACRYLIC : crop1 = 4800, crop2 = 1224
+
+opt_overlap1, opt_overlap2_1 = sweep_and_compare(img1, img2, 4150, 1874) 
+opt_overlap2_3, opt_overlap3_2 = sweep_and_compare(img2, img3, 4150, 1874)
+opt_overlap3_4, opt_overlap4_3 = sweep_and_compare(img3, img4, 4150, 1874)
+opt_overlap4_5, opt_overlap5_4 = sweep_and_compare(img4, img5, 4150, 1874)
+opt_overlap5_6, opt_overlap6_5 = sweep_and_compare(img5, img6, 4150, 1874)
+opt_overlap6_7, opt_overlap7 = sweep_and_compare(img6, img7, 4150, 1874)
+
+#TRY CHANGING THRESHOLD TO 5 FOR EXAMPLE
+
+cv2.imshow("opt_overlap5_6", opt_overlap5_6)
+cv2.imwrite("opt_overlap5_6.png", opt_overlap5_6)
+cv2.waitKey()
+
+cv2.imshow("opt_overlap6_5", opt_overlap6_5)
+cv2.imwrite("opt_overlap6_5.png", opt_overlap6_5)
 cv2.waitKey()
 
 
-#p1 = align_images_hor(img1, img2, opt_overlap1, opt_overlap2_1) 
-#cv2.imshow("partial1", p1)
-#cv2.imwrite("partial1.png", p1)
-#cv2.waitKey()
-
-p2 = align_row(img1, img2, img3, opt_overlap1, opt_overlap2_1, opt_overlap2_3, opt_overlap3)
-cv2.imshow("partial2", p2)
-cv2.imwrite("partial2.png", p2)
+final_row = align_row(img1, img2, img3, img4, img5, img6, img7, 
+               opt_overlap1, opt_overlap2_1, opt_overlap2_3, opt_overlap3_2,
+               opt_overlap3_4, opt_overlap4_3, opt_overlap4_5, opt_overlap5_4,
+               opt_overlap5_6, opt_overlap6_5, opt_overlap6_7, opt_overlap7)
+cv2.imshow("final_row_oak", final_row)
+cv2.imwrite("final_row_oak.png", final_row)
 cv2.waitKey()
