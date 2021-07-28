6ac1d6c7b872a6f1fa99927fee9c37d1c4d5901e
Syrine28 <84201132+Syrine28@users.noreply.github.com>
2021-07-28 20:14:21 +0100

    Update opt_overlapping_reg_w_offset.py

--- opt_overlapping_reg_w_offset.py
+++ opt_overlapping_reg_w_offset.py
@@ -11,14 +11,23 @@ def normal_vector(pixels) :
 
 def convert_to_array_hor_al(crop, img) :
     col_pixel = []
-    for i in range(0,img.shape[0]):
+    for i in range(0, img.shape[0]):
         pixel = img[i, crop]/255
         col_pixel.append(pixel)
     return col_pixel 
 
+def mean_normals(img) :
+    pixels = []
+    for i in range(0, img.shape[0]) :
+        for j in range(0, img.shape[1]) :
+            pixel = img[i, j]/255
+            pixels.append(pixel)
+    return np.mean(pixels)
+    
+
 def convert_to_array_ver_al(crop, img) :
     row_pixel = []
-    for i in range(0,img.shape[1]):
+    for i in range(0, img.shape[1]):
         pixel = img[crop, i]/255
         row_pixel.append(pixel)
     return row_pixel 
@@ -40,9 +49,11 @@ def cma_hor(img1, img2, crop1) :
 
     fc_min = cma.fmin(f, 2 * [crop1], 10)
    
+    """
     print("CMA")
     print(fc_min)
-
+    """
+    
 def sweep_and_compare_hor(img1, img2, crop1)  :
     
     results = []
@@ -72,10 +83,6 @@ def sweep_and_compare_hor(img1, img2, crop1)  :
         
     plt.plot(shifts, results)
     plt.show()
-    
-    print("")
-    print("STANDARD")
-    print(offset)
     
     return offset
         
@@ -194,10 +201,43 @@ img14 = cv2.imread("C:\\Users\\mocap\\Desktop\\pvc\\pvc_13_heightmap_nrm_scaled.
 img15 = cv2.imread("C:\\Users\\mocap\\Desktop\\pvc\\pvc_14_heightmap_nrm_scaled.png")
 
 
+# scaled oak
+"""
+img1 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_00_heightmap_nrm_scaled.png")
+ 
+img2 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_01_heightmap_nrm_scaled.png")
+
+img3 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_02_heightmap_nrm_scaled.png")
+
+img4 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_03_heightmap_nrm_scaled.png")
+
+img5 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_04_heightmap_nrm_scaled.png")
+
+img6 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_05_heightmap_nrm_scaled.png")
+
+img7 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_06_heightmap_nrm_scaled.png")
+
+img8 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_07_heightmap_nrm_scaled.png")
+
+img9 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_08_heightmap_nrm_scaled.png")
+ 
+img10 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_09_heightmap_nrm_scaled.png")
+
+img11 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_10_heightmap_nrm_scaled.png")
+
+img12 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_11_heightmap_nrm_scaled.png")
+
+img13 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_12_heightmap_nrm_scaled.png")
+
+img14 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_13_heightmap_nrm_scaled.png")
+
+img15 = cv2.imread("C:\\Users\\mocap\\Desktop\\oak\\oak_14_heightmap_nrm_scaled.png")
+"""
+
 #THIS IS FOR SECOND METHOD
 
 #THIS IS FOR NON-SCALED IMAGES
-"""
+
 offset_hor1 = sweep_and_compare_hor(img1, img2, 4700, 1324) 
 offset_hor2 = sweep_and_compare_hor(img2, img3, 4700, 1324)
 offset_hor3 = sweep_and_compare_hor(img3, img4, 4700, 1324)
@@ -235,17 +275,13 @@ img_right4, img_down4 = align_right_and_down(img4, img5, img11, offset_hor4, off
 img_right5, img_down5 = align_right_and_down(img5, img6, img12, offset_hor5, offset_ver5)
 img_right6, img_down6 = align_right_and_down(img6, img7, img13, offset_hor6, offset_ver6)
 img_down7 = align_images_ver(img7, img14, offset_ver7)
-"""
 
-offset_hor1_cma = cma_hor(img1, img2, 2000)
-
-"""
-cv2.imshow("pvc_down", img_down3)
-cv2.imwrite("pvc_down.png", img_down3)
+cv2.imshow("pvc_down7", img_down7)
+cv2.imwrite("pvc_down7.png", img_down7)
 cv2.waitKey()
 
-cv2.imshow("pvc_down1", img_down2)
-cv2.imwrite("pvc_down1.png", img_down2)
+cv2.imshow("pvc_right5", img_right5)
+cv2.imwrite("pvc_right5.png", img_right5)
 cv2.waitKey()
 """
 
@@ -253,7 +289,6 @@ cv2.waitKey()
 """
 final_img = align_row(img_right1, img_right2, img_right3, img_right4, img_right5, img_right6, offset_hor2, offset_hor3, offset_hor4, offset_hor5, offset_hor6)
 
-cv2.imshow("final_acry_scaled", final_img)
-cv2.imwrite("final_acry_scaled.png", final_img)
+cv2.imshow("final_oak_scaled", final_img)
+cv2.imwrite("final_oak_scaled.png", final_img)
 cv2.waitKey()
-"""
